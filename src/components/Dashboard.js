8959be9a8eb63106ade4cadfdcc3e80be653e009
Jenessa White <jenessaw31@gmail.com>
2020-03-26 21:18:42 -0500

    we've got data rows

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -35,8 +35,49 @@ class Dashboard extends Component {
     const { data } = this.state;
 
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="container py-lg-3">
+        <div className="row">
+          <div className="col-12">
+            <h1>Dashboard</h1>
+            <div className="row">
+              {
+                data.map(item => {
+                  let keys = Object.keys(item.data[0])
+
+                  return (
+                    <div className="col-12" key={item.name}>
+                      <div className="card">
+                        <div className="card-body">
+                          <h4 className="card-title">{item.name}</h4>
+                        </div>
+                        <table className="table">
+                          <thead>
+                            <tr>
+                            { keys.map(title => (
+                              <th scope="col" key={title}>{title}</th>
+                            ))}
+                            </tr>
+                          </thead>
+
+                          <tbody>
+                          { item.data.map((values, key) => (
+                            <tr key={key}>
+                              { Object.values(values).map((value, key) => (
+                                <td key={key}>{value}</td>
+                              ))}
+                            </tr>
+                          ))}
+                          </tbody>
+                        </table>
+
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
